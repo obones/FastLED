4016e64463f4f943256eb43a87b909782fe41b9b
OBones <obones@free.fr>
2025-11-18 22:45:18 +0100

    fix: mutualize pin settings + add "direct code" for pin setting (commented out)

--- src/platforms/arm/max32/fastpin_arm_max32.h
+++ src/platforms/arm/max32/fastpin_arm_max32.h
@@ -22,16 +22,20 @@ public:
 	typedef volatile uint32_t * port_ptr_t;
 	typedef uint32_t port_t;
 
-	inline static void setOutput() { mxc_gpio_cfg_t pinConfig = {PORT(), MASK, MXC_GPIO_FUNC_OUT, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO, MXC_GPIO_DRVSTR_0}; MXC_GPIO_Config(&pinConfig); }
-	inline static void setInput() { mxc_gpio_cfg_t pinConfig = {PORT(), MASK, MXC_GPIO_FUNC_IN, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIO, MXC_GPIO_DRVSTR_0}; MXC_GPIO_Config(&pinConfig); }
+	inline static void setPinFunction(mxc_gpio_func_t function) {
+		mxc_gpio_cfg_t pinConfig = {PORT(), MASK, function, MXC_GPIO_PAD_NONE, MXC_GPIO_VSSEL_VDDIOH, MXC_GPIO_DRVSTR_3};
+		MXC_GPIO_Config(&pinConfig);
+	}
+	inline static void setOutput() { setPinFunction(MXC_GPIO_FUNC_OUT); }
+	inline static void setInput() { setPinFunction(MXC_GPIO_FUNC_IN); }
 
-    inline static void hi() __attribute__ ((always_inline)) { MXC_GPIO_OutSet(PORT(), MASK); }
-	inline static void lo() __attribute__ ((always_inline)) { MXC_GPIO_OutClr(PORT(), MASK); }
-	inline static void set(FASTLED_REGISTER port_t val) __attribute__ ((always_inline)) { MXC_GPIO_OutPut(PORT(), MASK, val); }
+    inline static void hi() __attribute__ ((always_inline)) { MXC_GPIO_OutSet(PORT(), MASK); /* PORT()->out_set = MASK;*/ }
+	inline static void lo() __attribute__ ((always_inline)) { MXC_GPIO_OutClr(PORT(), MASK); /*PORT()->out_clr = MASK;*/ }
+	inline static void set(FASTLED_REGISTER port_t val) __attribute__ ((always_inline)) { MXC_GPIO_OutPut(PORT(), MASK, val); /*PORT()->out = (PORT()->out & ~MASK) | (val & MASK);*/ }
 
 	inline static void strobe() __attribute__ ((always_inline)) { toggle(); toggle(); }
 
-	inline static void toggle() __attribute__ ((always_inline)) { MXC_GPIO_OutToggle(PORT(), MASK); }
+	inline static void toggle() __attribute__ ((always_inline)) { MXC_GPIO_OutToggle(PORT(), MASK); /*PORT()->out ^= MASK;*/ }
 
 	inline static void hi(FASTLED_REGISTER port_ptr_t port) __attribute__ ((always_inline)) { hi(); }
 	inline static void lo(FASTLED_REGISTER port_ptr_t port) __attribute__ ((always_inline)) { lo(); }
